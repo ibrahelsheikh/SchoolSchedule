914c526a8bbc97c03a603e830db70b1854d3c495
Ibrahim Elsheikh <ibrahimelsheik2020@gmail.com>
2025-12-17 22:55:50 +0200

    init commit

---
+++ src/main/java/com/example/demo/entity/assignTeacherToSubject.java
@@ -0,0 +1,20 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class assignTeacherToSubject {
+    @Id
+    private Long id;
+
+    private Long teacherId;
+    private Long subjectId;
+
+}
