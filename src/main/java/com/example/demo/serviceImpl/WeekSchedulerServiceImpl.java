31a68eda34129391c5dcdb1bd8e24fe9326fc98c
Ibrahim Elsheikh <ibrahimelsheik2020@gmail.com>
2025-12-24 17:04:13 +0200

    init commit

--- src/main/java/com/example/demo/serviceImpl/WeekSchedulerServiceImpl.java
+++ src/main/java/com/example/demo/serviceImpl/WeekSchedulerServiceImpl.java
@@ -2,14 +2,11 @@ package com.example.demo.serviceImpl;
 
 import com.example.demo.dto.request.CreateWeeklyScheduleRequest;
 import com.example.demo.dto.resonpse.WeeklyScheduleResponse;
-import com.example.demo.entity.Subject;
-import com.example.demo.entity.Teacher;
 import com.example.demo.entity.WeeklySchedule;
 import com.example.demo.repository.SubjectRepository;
 import com.example.demo.repository.TeacherRepository;
 import com.example.demo.service.WeekSchedulerService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Service;
 
 import java.awt.print.Pageable;
@@ -27,15 +24,15 @@ public class WeekSchedulerServiceImpl implements WeekSchedulerService {
     public WeeklySchedule create(CreateWeeklyScheduleRequest request) {
 
 
-        Subject subject = subjectRepository.findById(request.)
-                .orElseThrow(() -> new ChangeSetPersister.NotFoundException("Subject not found"));
-
-        Teacher teacher = teacherRepository.findById(request.teacherId())
-                .orElseThrow(() -> new ChangeSetPersister.NotFoundException("Teacher not found"));
-
-        lesson.setSubject(subject);
-        lesson.setTeacher(teacher);
-
+//        Subject subject = subjectRepository.findById(request.)
+//                .orElseThrow(() -> new ChangeSetPersister.NotFoundException("Subject not found"));
+//
+//        Teacher teacher = teacherRepository.findById(request.teacherId())
+//                .orElseThrow(() -> new ChangeSetPersister.NotFoundException("Teacher not found"));
+//
+//        lesson.setSubject(subject);
+//        lesson.setTeacher(teacher);
+//
 
 
         return null;
