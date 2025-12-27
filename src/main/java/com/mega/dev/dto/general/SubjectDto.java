bebaff8c6a332db52cee7f470af9bf4ec288a8eb
Ibrahim Elsheikh <ibrahimelsheik2020@gmail.com>
2025-12-27 12:20:13 +0200

    init commit

--- src/main/java/com/mega/dev/dto/general/SubjectDto.java
+++ src/main/java/com/mega/dev/dto/general/SubjectDto.java
@@ -3,10 +3,12 @@ package com.mega.dev.dto.general;
 import com.mega.dev.emuns.Grade;
 import com.mega.dev.emuns.Semester;
 
+import java.util.List;
+
 
 public record SubjectDto(
         Long id,
         String name,
-        Semester semester,
+        List<Semester> semester,
         Grade grade
 ) {}
